1bd77002b0ad42ad72b5050545f2a21d72716e2c
Saikiran Gunturu <saikirangunturi@gmail.com>
2026-05-30 12:06:43 +0530

    Refactor proc_load_silver for data normalization
    Refactor SQL procedure to normalize company names and trim whitespace for various fields. Removed redundant case statements and added currency specification for dividends.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -43,20 +43,10 @@ BEGIN
 			country
 		)
 		SELECT DISTINCT
-			company_id,
-			CASE	UPPER(TRIM(company_name))
-					WHEN 'TCS' THEN 'Tata Consultancy Services Limited'
-					WHEN 'INFY' THEN 'Infosys Limited'
-					WHEN 'RELIANCE' THEN 'Reliance Industries Limited'
-					WHEN 'HDFCBANK' THEN 'HDFC Bank Limited'
-					WHEN 'ICICIBANK' THEN 'ICICI Bank Limited'
-					WHEN 'WIPRO' THEN 'Wipro Limited'
-					WHEN 'SBIN' THEN 'State Bank of India Limited'
-					WHEN 'AXISBANK' THEN 'Axis Bank Limited'
-					ELSE 'n/a'
-			END company_name,   -- Normalize company names to readable format
-			sector,
-			country
+			TRIM(company_id) AS company_id,
+			UPPER(TRIM(company_name)) AS company_name,
+			TRIM(sector) AS sector,
+			TRIM(country) AS country
 			FROM bronze.mdv_companies
 			WHERE company_id IS NOT NULL;
 		SET @end_time = GETDATE();
@@ -75,10 +65,10 @@ BEGIN
 			company_id
 		)
 		SELECT DISTINCT
-			security_id,
-			ticker,
-			isin,
-			company_id
+			TRIM(security_id) AS security_id,
+			TRIM(ticker) AS ticker,
+			TRIM(isin) AS isin,
+			TRIM(company_id) AS company_id
 		FROM bronze.mdv_securities
 		WHERE security_id IS NOT NULL
 			AND company_id IS NOT NULL;
@@ -96,243 +86,14 @@ BEGIN
 			event_name
 		)
 		SELECT
-			event_type_id,
-			event_name
+			TRIM(event_type_id) AS event_type_id,
+			TRIM(event_name) AS event_name
 		FROM bronze.mdv_event_types
 		WHERE event_type_id IS NOT NULL;
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
-
-        -- Loading corporate actions data
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.mdv_corporate_actions';
-		TRUNCATE TABLE silver.mdv_corporate_actions;
-		PRINT '>> Inserting Data Into: silver.mdv_corporate_actions';
-		WITH bronze2silver_corporate_actions AS
-		(
-			SELECT
-				TRY_CAST(action_id AS INT) AS action_id,
-				UPPER(TRIM(ticker)) AS ticker,
-
-				CASE
-					WHEN event_type IN ('DIV', 'Dividend', 'Cash Dividend') THEN 'Dividend'
-					WHEN event_type IN ('Split', 'Stock Split') THEN 'Split'
-					WHEN event_type = 'MERGER' THEN 'Merger'
-					ELSE 'n/a'
-				END AS event_type,
-
-				COALESCE(
-					TRY_CONVERT(DATE, NULLIF(TRIM(announcement_date), ''), 105), -- dd-mm-yyyy
-					TRY_CONVERT(DATE, NULLIF(TRIM(announcement_date), ''), 23),  -- yyyy-mm-dd
-					TRY_CONVERT(DATE, NULLIF(TRIM(announcement_date), ''), 103), -- dd/mm/yyyy
-					TRY_CONVERT(DATE, NULLIF(TRIM(announcement_date), ''), 111)  -- yyyy/mm/dd
-					) AS announcement_date,
-				
-				COALESCE(
-					TRY_CONVERT(DATE, NULLIF(TRIM(ex_date), ''), 105),
-					TRY_CONVERT(DATE, NULLIF(TRIM(ex_date), ''), 23),
-					TRY_CONVERT(DATE, NULLIF(TRIM(ex_date), ''), 103),
-					TRY_CONVERT(DATE, NULLIF(TRIM(ex_date), ''), 111)
-					) AS ex_date,
-
-				COALESCE(
-					TRY_CONVERT(DATE, NULLIF(TRIM(record_date), ''), 105),
-					TRY_CONVERT(DATE, NULLIF(TRIM(record_date), ''), 23),
-					TRY_CONVERT(DATE, NULLIF(TRIM(record_date), ''), 103),
-					TRY_CONVERT(DATE, NULLIF(TRIM(record_date), ''), 111)
-					) AS record_date,
-
-				COALESCE(
-				TRY_CONVERT(DATE, NULLIF(TRIM(payment_date), ''), 105),
-				TRY_CONVERT(DATE, NULLIF(TRIM(payment_date), ''), 23),
-				TRY_CONVERT(DATE, NULLIF(TRIM(payment_date), ''), 103),
-				TRY_CONVERT(DATE, NULLIF(TRIM(payment_date), ''), 111)
-				) AS payment_date,
-
-				TRY_CAST(NULLIF(TRIM(dividend_amount), '') AS DECIMAL(10,2)) AS dividend_amount,
-
-				CASE
-					WHEN split_ratio LIKE '%:%'
-							THEN CONCAT(TRY_CONVERT(INT, LEFT(split_ratio, CHARINDEX(':', split_ratio) - 1)),
-								':',
-								TRY_CONVERT(INT, RIGHT(split_ratio, LEN(split_ratio) - CHARINDEX(':', split_ratio))
-								))
-					ELSE NULL
-				END AS split_ratio,
-
-				CASE
-					WHEN status IS NULL OR TRIM(REPLACE(REPLACE(status, CHAR(13), ''), CHAR(10), '')) = '' THEN 'UNKNOWN'
-					ELSE UPPER(TRIM(REPLACE(REPLACE(status, CHAR(13), ''), CHAR(10), '')))
-				END AS ca_status
-				FROM bronze.mdv_corporate_actions
-				WHERE TRY_CAST(action_id AS INT) IS NOT NULL
-				AND TRY_CAST(action_id AS INT) > 0
-		),
-		mapped_corporate_actions AS
-		(
-			SELECT
-			ca.*,
-			c.company_id,
-			c.company_name,
-			s.security_id,
-			s.ticker AS master_ticker,
-			et.event_type_id
-		FROM bronze2silver_corporate_actions ca
-		INNER JOIN silver.mdv_securities s
-		ON ca.ticker = s.ticker
-		INNER JOIN silver.mdv_companies c
-		ON s.company_id = c.company_id
-		INNER JOIN silver.mdv_event_types et
-		ON ca.event_type = et.event_name
-		)
-		INSERT INTO silver.mdv_corporate_actions
-		(
-			action_id,
-			company_id,
-			security_id,
-			event_type_id,
-			company_name,
-			ticker,
-			event_type,
-			announcement_date,
-			ex_date,
-			record_date,
-			payment_date,
-			dividend_amount,
-			split_ratio,
-			ca_status
-		)
-		SELECT DISTINCT
-			action_id,
-			company_id,
-			security_id,
-			event_type_id,
-			company_name,
-			ticker,
-			event_type,
-			announcement_date,
-			ex_date,
-			record_date,
-			payment_date,
-
-			CASE
-				WHEN event_type = 'Dividend' THEN dividend_amount
-				ELSE NULL
-			END AS dividend_amount,
-
-			CASE
-				WHEN event_type = 'Split' THEN split_ratio
-				ELSE NULL
-			END AS split_ratio,
-
-			ca_status
-		FROM mapped_corporate_actions
-		WHERE action_id IS NOT NULL
-			AND company_id IS NOT NULL
-			AND security_id IS NOT NULL
-			AND event_type_id IS NOT NULL
-			AND event_type IN ('Dividend', 'Split', 'Merger')
-			AND announcement_date IS NOT NULL
-			/* Date validations */
-			AND (ex_date IS NULL OR ex_date >= announcement_date)
-			AND (record_date IS NULL OR record_date >= announcement_date)
-			AND (payment_date IS NULL OR payment_date >= announcement_date)
-			AND (ex_date IS NULL OR record_date IS NULL OR record_date >= ex_date)
-			AND (ex_date IS NULL OR payment_date IS NULL OR payment_date >= ex_date)
-			AND (record_date IS NULL OR payment_date IS NULL OR payment_date >= record_date)
-			/* Event-specific validations */
-			AND (event_type <> 'Dividend' OR dividend_amount IS NOT NULL)
-			AND (event_type <> 'Split' OR split_ratio IS NOT NULL)
-			AND NOT (event_type = 'Dividend' AND split_ratio IS NOT NULL)
-			AND NOT (event_type = 'Split' AND dividend_amount IS NOT NULL);
-
-	    SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
-
-		-- Loading dividends data
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.mdv_dividends';
-		TRUNCATE TABLE silver.mdv_dividends;
-		PRINT '>> Inserting Data Into: silver.mdv_dividends';
-		INSERT INTO silver.mdv_dividends (
-				action_id,
-				dividend_amount,
-				currency			
-		)
-		SELECT DISTINCT
-			TRY_CONVERT(INT, d.action_id) AS action_id,
-			TRY_CONVERT(DECIMAL(18,2), NULLIF(TRIM(d.dividend_amount), '')) AS dividend_amount,
-			UPPER(TRIM(d.currency)) AS currency
-			FROM bronze.mdv_dividends d
-			INNER JOIN silver.mdv_corporate_actions ca
-			ON TRY_CONVERT(INT, d.action_id) = ca.action_id
-			INNER JOIN silver.mdv_event_types et
-			ON ca.event_type_id = et.event_type_id
-			WHERE et.event_name = 'Dividend'
-			AND TRY_CONVERT(INT, d.action_id) IS NOT NULL
-			AND TRY_CONVERT(DECIMAL(18,2), NULLIF(TRIM(d.dividend_amount), '')) IS NOT NULL
-			AND NULLIF(TRIM(d.currency), '') IS NOT NULL;
-
-		SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
-
-		-- Loading splits data
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.mdv_splits';
-		TRUNCATE TABLE silver.mdv_splits;
-		PRINT '>> Inserting Data Into: silver.mdv_splits';
-		INSERT INTO silver.mdv_splits (
-				action_id,
-				split_ratio
-			)
-		SELECT DISTINCT
-				TRY_CONVERT(INT, s.action_id) AS action_id,
-				NULLIF(TRIM(s.split_ratio), '') AS split_ratio
-		FROM bronze.mdv_splits s
-		INNER JOIN silver.mdv_corporate_actions ca
-		ON TRY_CONVERT(INT, s.action_id) = ca.action_id
-		INNER JOIN silver.mdv_event_types et
-		ON ca.event_type_id = et.event_type_id
-		WHERE et.event_name = 'Split'
-		AND TRY_CONVERT(INT, s.action_id) IS NOT NULL
-		AND NULLIF(TRIM(s.split_ratio), '') IS NOT NULL;
-
-	    SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
-
-		-- Loading mergers data
-        SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.mdv_mergers';
-		TRUNCATE TABLE silver.mdv_mergers;
-		PRINT '>> Inserting Data Into: silver.mdv_mergers';
-		INSERT INTO silver.mdv_mergers (
-				action_id,
-				target_company,
-				acquirer_company
-			)
-		SELECT DISTINCT
-				TRY_CONVERT(INT, m.action_id) AS action_id,
-				UPPER(TRIM(m.target_company)) AS target_company,
-				UPPER(TRIM(m.acquirer_company)) AS acquirer_company
-		FROM bronze.mdv_mergers m
-		INNER JOIN silver.mdv_corporate_actions ca
-		ON TRY_CONVERT(INT, m.action_id) = ca.action_id
-		INNER JOIN silver.mdv_event_types et
-		ON ca.event_type_id = et.event_type_id
-		WHERE et.event_name = 'Merger'
-		AND TRY_CONVERT(INT, m.action_id) IS NOT NULL
-		AND NULLIF(TRIM(m.target_company), '') IS NOT NULL
-		AND NULLIF(TRIM(m.acquirer_company), '') IS NOT NULL
-		AND UPPER(TRIM(m.target_company)) <> UPPER(TRIM(m.acquirer_company));
-
-		SET @end_time = GETDATE();
-        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
-
+		
 		-- Loading dates data
         SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: silver.mdv_dates';
@@ -358,8 +119,8 @@ BEGIN
 					TRY_CAST(year AS INT) AS year,
 
 					CASE
-						WHEN UPPER(TRIM(is_weekend)) = 'Y' THEN 'Y'
-						WHEN UPPER(TRIM(is_weekend)) = 'N' THEN 'N'
+						WHEN day_of_week IN (6,7) THEN 'Y'
+						WHEN day_of_week BETWEEN 1 AND 5 THEN 'N'
 						ELSE 'UNKNOWN'
 					END AS is_weekend
 				FROM bronze.mdv_dates
@@ -392,6 +153,242 @@ BEGIN
 			WHERE full_date IS NOT NULL;
 
 	    SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+		
+		-- Loading dividends data
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.mdv_dividends';
+		TRUNCATE TABLE silver.mdv_dividends;
+		PRINT '>> Inserting Data Into: silver.mdv_dividends';
+			INSERT INTO silver.mdv_dividends (
+					action_id,
+					dividend_amount,
+					currency			
+			)
+			SELECT DISTINCT
+				TRY_CONVERT(INT, TRIM(action_id)) AS action_id,
+				TRY_CONVERT(DECIMAL(18,2), NULLIF(TRIM(dividend_amount), '')) AS dividend_amount,
+				'INR' AS currency
+				FROM bronze.mdv_dividends
+				WHERE TRY_CONVERT(INT, TRIM(action_id)) IS NOT NULL
+				AND TRY_CONVERT(DECIMAL(18,2), NULLIF(TRIM(dividend_amount), '')) IS NOT NULL;
+
+		SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+
+		-- Loading splits data
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.mdv_splits';
+		TRUNCATE TABLE silver.mdv_splits;
+		PRINT '>> Inserting Data Into: silver.mdv_splits';
+		INSERT INTO silver.mdv_splits (
+				action_id,
+				split_ratio
+			)
+		SELECT DISTINCT
+				TRY_CONVERT(INT, TRIM(action_id)) AS action_id,
+				REPLACE(REPLACE(TRIM(split_ratio), '02:01', '2:1'), '05:01', '5:1') AS split_ratio
+		FROM bronze.mdv_splits
+		WHERE TRY_CONVERT(INT, TRIM(action_id)) IS NOT NULL
+		AND NULLIF(TRIM(split_ratio), '') IS NOT NULL;
+
+	    SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+
+		-- Loading mergers data
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.mdv_mergers';
+		TRUNCATE TABLE silver.mdv_mergers;
+		PRINT '>> Inserting Data Into: silver.mdv_mergers';
+		WITH merger_dedup AS
+		(
+		SELECT
+        TRY_CONVERT(INT, TRIM(action_id)) AS action_id,
+        UPPER(TRIM(target_company)) AS target_company,
+        UPPER(TRIM(acquirer_company)) AS acquirer_company,
+        ROW_NUMBER() OVER (
+            PARTITION BY
+                UPPER(TRIM(target_company)),
+                UPPER(TRIM(acquirer_company))
+            ORDER BY TRY_CONVERT(INT, TRIM(action_id))
+        ) AS rn
+		FROM bronze.mdv_mergers
+		WHERE TRY_CONVERT(INT, TRIM(action_id)) IS NOT NULL
+		AND NULLIF(TRIM(target_company), '') IS NOT NULL
+		AND NULLIF(TRIM(acquirer_company), '') IS NOT NULL
+		AND UPPER(TRIM(target_company)) <> UPPER(TRIM(acquirer_company))
+		)
+		INSERT INTO silver.mdv_mergers (
+			action_id,
+			target_company,
+			acquirer_company
+		)
+		SELECT
+			action_id,
+			target_company,
+			acquirer_company
+		FROM merger_dedup
+		WHERE rn = 1;
+
+		SET @end_time = GETDATE();
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        PRINT '>> -------------';
+
+		-- Loading corporate actions data
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.mdv_corporate_actions';
+		TRUNCATE TABLE silver.mdv_corporate_actions;
+		PRINT '>> Inserting Data Into: silver.mdv_corporate_actions';
+		WITH bronze2silver_corporate_actions AS
+		(
+			SELECT
+				ca.*,
+				TRY_CONVERT(INT, TRIM(ca.action_id)) AS clean_action_id,
+				UPPER(TRIM(ca.company_name)) AS clean_company_name,
+				UPPER(TRIM(ca.ticker)) AS clean_ticker,
+				UPPER(TRIM(ca.event_type)) AS clean_event_type_raw,
+				TRY_CONVERT(DATE, NULLIF(TRIM(ca.announcement_date), ''), 105) AS clean_announcement_date,
+				TRY_CONVERT(DATE, NULLIF(TRIM(ca.ex_date), ''), 105) AS clean_ex_date,
+				TRY_CONVERT(DATE, NULLIF(TRIM(ca.record_date), ''), 105) AS clean_record_date,
+				TRY_CONVERT(DATE, NULLIF(TRIM(ca.payment_date), ''), 105) AS clean_payment_date
+
+			FROM bronze.mdv_corporate_actions ca
+			),
+			mapped_corporate_actions AS
+			(
+				SELECT
+					ca.*,
+					c.company_name AS master_company_name,
+
+					CASE
+						WHEN clean_event_type_raw IN ('DIV', 'DIVIDEND', 'CASH DIVIDEND') THEN 'Dividend'
+						WHEN clean_event_type_raw IN ('SPLIT', 'STOCK SPLIT') THEN 'Split'
+						WHEN clean_event_type_raw IN ('MERGER', 'M&A', 'MERGERS') THEN 'Merger'
+						ELSE 'Invalid'
+					END AS event_type_standard,
+
+					CASE
+						WHEN clean_action_id IS NULL THEN 1
+						ELSE 0
+					END AS is_action_id_missing,
+
+					CASE
+						WHEN clean_event_type_raw NOT IN
+						(
+							'DIV', 'DIVIDEND', 'CASH DIVIDEND',
+							'SPLIT', 'STOCK SPLIT',
+							'MERGER', 'M&A', 'MERGERS'
+						)
+						THEN 1
+						ELSE 0
+					END AS is_event_type_invalid,
+
+					CASE
+						WHEN s.ticker IS NULL THEN 1
+						ELSE 0
+					END AS is_company_ticker_mismatch,
+
+					CASE
+						WHEN clean_ex_date < clean_announcement_date
+						OR clean_record_date < clean_ex_date
+						OR clean_payment_date < clean_record_date
+						THEN 1 
+						ELSE 0
+					END AS is_date_sequence_invalid,
+
+					CASE
+						WHEN clean_announcement_date IS NULL
+						OR clean_ex_date IS NULL
+						OR clean_record_date IS NULL
+						OR clean_payment_date IS NULL
+						THEN 1
+						ELSE 0
+					END AS is_mandatory_date_missing
+				FROM bronze2silver_corporate_actions ca
+				LEFT JOIN silver.mdv_companies c
+				    ON ca.clean_company_name = UPPER(TRIM(c.company_name))
+				LEFT JOIN silver.mdv_securities s
+					ON c.company_id = s.company_id
+					AND ca.clean_ticker = UPPER(TRIM(s.ticker))
+				)
+
+				INSERT INTO silver.mdv_corporate_actions
+				(
+					action_id,
+					company_name,
+					ticker,
+					master_company_name,
+					event_type_raw,
+					event_type_standard,
+					announcement_date,
+					ex_date,
+					record_date,
+					payment_date,
+					dividend_amount,
+					split_ratio,
+					ca_status,
+					is_action_id_missing,
+					is_event_type_invalid,
+					is_company_ticker_mismatch,
+					is_date_sequence_invalid,
+					is_mandatory_date_missing,
+					validation_status,
+					validation_message
+				)
+				SELECT DISTINCT
+					clean_action_id,
+					clean_company_name,
+					clean_ticker,
+					master_company_name,
+					clean_event_type_raw,
+					event_type_standard,
+					clean_announcement_date,
+					clean_ex_date,
+					clean_record_date,
+					clean_payment_date,
+
+					TRY_CONVERT(DECIMAL(18,2), NULLIF(TRIM(dividend_amount), '')) AS dividend_amount,
+			
+					CASE
+						WHEN NULLIF(TRIM(split_ratio), '') IS NULL THEN NULL
+						ELSE REPLACE(REPLACE(TRIM(split_ratio), '02:01', '2:1'), '05:01', '5:1')
+					END AS split_ratio,
+
+					CASE
+						WHEN NULLIF(TRIM(status), '') IS NULL THEN NULL
+						ELSE UPPER(LEFT(TRIM(status),1))
+							+ LOWER(SUBSTRING(TRIM(status),2,LEN(TRIM(status))))
+					END AS ca_status,
+
+					is_action_id_missing,
+					is_event_type_invalid,
+					is_company_ticker_mismatch,
+					is_date_sequence_invalid,
+					is_mandatory_date_missing,
+
+					CASE
+						WHEN is_action_id_missing = 1 THEN 'Invalid'
+						WHEN is_event_type_invalid = 1 THEN 'Invalid'
+						WHEN is_company_ticker_mismatch = 1 THEN 'Invalid'
+						WHEN is_date_sequence_invalid = 1 THEN 'Invalid'
+						WHEN is_mandatory_date_missing = 1 THEN 'Warning'
+						ELSE 'Valid'
+					END AS validation_status,
+
+					CASE
+						WHEN is_action_id_missing = 1 THEN 'Invalid action id'
+						WHEN is_event_type_invalid = 1 THEN 'Invalid event type'
+						WHEN is_company_ticker_mismatch = 1 THEN 'Company and ticker mismatch'
+						WHEN is_date_sequence_invalid = 1 THEN 'Invalid date sequence'
+						WHEN is_mandatory_date_missing = 1 THEN 'Mandatory date missing'
+						ELSE 'Valid record'
+					END AS validation_message
+				FROM mapped_corporate_actions;
+				
+		SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
 
